48948bb87f44f35c95a7959e0475f28e5d88c892
Lokesh <lokeshchauhan9923@gmail.com>
2020-10-18 12:12:45 +0530

    Final

---
+++ app/src/main/java/com/example/firebasedownload/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.firebasedownload;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    private StorageReference mStorageRef;
+    private ImageView imageView;
+    private Button btnDownload;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+        btnDownload = findViewById(R.id.download);
+
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+
+        btnDownload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://firebasestorage.googleapis.com/v0/b/fir-download-1fac4.appspot.com/o/edited.jpg?alt=media&token=256ebbb2-6e3a-40ad-bdd3-199fc8cc8452";
+                downloadFile(MainActivity.this,"1",".img","My Folder",url);
+                Picasso.get().load(url).resize(1024,1024).into(imageView);
+            }
+        });
+
+    }
+
+    public long downloadFile(Context context, String fileName, String fileExtension, String destinationDirectory, String url) {
+
+
+        DownloadManager downloadmanager = (DownloadManager) context.
+                getSystemService(Context.DOWNLOAD_SERVICE);
+        Uri uri = Uri.parse(url);
+        DownloadManager.Request request = new DownloadManager.Request(uri);
+
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        request.setDestinationInExternalFilesDir(context, destinationDirectory, fileName + fileExtension);
+
+        return downloadmanager.enqueue(request);
+    }
+}
